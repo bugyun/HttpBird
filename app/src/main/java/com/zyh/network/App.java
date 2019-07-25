94843bd3bba775debeb662486482bb76576839d5
若云 <zyhdvlp@gmail.com>
2019-07-25 23:02:29 +0800

    去除.idea 忽略文件

--- app/src/main/java/com/zyh/network/App.java
+++ app/src/main/java/com/zyh/network/App.java
@@ -2,7 +2,6 @@ package com.zyh.network;
 
 import android.app.Application;
 
-import com.facebook.stetho.Stetho;
 
 /**
  * Created by ruoyun on 2017/3/31.
@@ -14,6 +13,5 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Stetho.initializeWithDefaults(this);
     }
 }
